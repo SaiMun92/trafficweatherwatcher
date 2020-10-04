b493fda91e04a20b3ca18567f63488a5516b9b57
sai mun <saimun92@gmail.com>
2020-10-04 16:56:40 +0800

    Updates

---
+++ precache-manifest.b6f1a4ae9288976d116c759c20edaa82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3513efea01b5a619104c168477153b7",
+    "url": "/trafficweatherwatcher/index.html"
+  },
+  {
+    "revision": "773131df6ba5b9e3a26f",
+    "url": "/trafficweatherwatcher/static/css/2.08c74b9a.chunk.css"
+  },
+  {
+    "revision": "289d982764e7479f0e18",
+    "url": "/trafficweatherwatcher/static/css/main.8758b7fb.chunk.css"
+  },
+  {
+    "revision": "773131df6ba5b9e3a26f",
+    "url": "/trafficweatherwatcher/static/js/2.d993a12e.chunk.js"
+  },
+  {
+    "revision": "ec64c3ac4c342d39c0735a19d18ad090",
+    "url": "/trafficweatherwatcher/static/js/2.d993a12e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "289d982764e7479f0e18",
+    "url": "/trafficweatherwatcher/static/js/main.701f7595.chunk.js"
+  },
+  {
+    "revision": "14de76e8702ba8c4e2fa",
+    "url": "/trafficweatherwatcher/static/js/runtime-main.df0d7975.js"
+  }
+]);
